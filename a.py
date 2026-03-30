b625a544543e8bd2aa79e18859eedfad02b5ade1
seanleeeeeeeeee <160643982+seanleeeeeeeeee@users.noreply.github.com>
2026-03-30 13:41:03 -0400

    Update a.py

--- a.py
+++ a.py
@@ -1,13 +1,13 @@
 from gpiozero import Motor
 import time
 motor = Motor(forward=12, backward=20)
-act = Motor(forward=5, backward=6)
+actuator = Motor(forward=5, backward=6)
 while True:
-    motor.forward(speed=0.3)
+    # motor.forward(speed=0.3)
+    # time.sleep(2)
+    # motor.backward(speed=0.3)
+    # time.sleep(2)
+    actuator.forward()
     time.sleep(2)
-    motor.backward(speed=0.3)
-    time.sleep(2)
-    act.forward()
-    time.sleep(2)
-    act.backward()
+    actuator.backward()
     time.sleep(2)
