8b4d9fb8bb6654a1b8e8e8d62bd3f87a709835fd
seanleeeeeeeeee <160643982+seanleeeeeeeeee@users.noreply.github.com>
2026-03-31 18:11:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ table = [[0 for _ in range(4)] for _ in range(20)]
 for i in range(20):
     buf = calc.calculate(i/20)
     length_l = length
-    angle_1 = angle
+    angle_l = angle
     length = buf["delta"]
     angle = buf["theta1"]
     # print("length", length, "angle", angle, "diff", length-length_l, angle-angle_l)
@@ -21,24 +21,41 @@ for i in range(20):
     table[i][2] = length-length_l
     table[i][3] = angle-angle_l
 
-
-def time_step(i):
+def actuator_step(i):
     if table[i][2] > 0:
         print("a+" + str(table[i][2]/7))
         actuator.forward()
     else:
         print("a-" + str(table[i][2]/7))
         actuator.backward()
+    time.sleep(abs(table[i][2]/7))
+    actuator.stop()
+
+def run_motor2():
     if table[i][3] > 0:
         print("m+")
         motor.forward()
     else:
         print("m-")
         motor.backward()
-    time.sleep(abs(table[i][2]/7))
+    time.sleep(abs(table[i][3]) * C)
     actuator.stop()
-    motor.stop()
+
+# Create threads
+thread1 = threading.Thread(target=run_motor1)
+thread2 = threading.Thread(target=run_motor2)
+
+# Start threads
+
+
+# Wait for both to finish
+
+def time_step(i):
+    thread1.start()
+    thread2.start()
     time.sleep(1)
+    thread1.join()
+    thread2.join()
     
 for row in table:
     for element in row:
