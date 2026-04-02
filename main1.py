979b434baab32c493134495b9cdeb680cefa07cc
seanleeeeeeeeee <160643982+seanleeeeeeeeee@users.noreply.github.com>
2026-04-02 00:52:18 -0400

    Update main1.py

--- main1.py
+++ main1.py
@@ -16,8 +16,8 @@ stopped = False
 motor = Motor(forward=12, backward=20)
 actuator = Motor(forward=6, backward=5)
 start = Button(1)
-stop = Button(1)
-manual = Button(1)
+stop = Button(2)
+manual = Button(3)
 
 def stop_brushing():
     stopped = True
