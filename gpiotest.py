f7c5daee39066edeff0796c675c0c667cd9cd439
seanleeeeeeeeee <160643982+seanleeeeeeeeee@users.noreply.github.com>
2026-04-08 23:02:57 -0400

    Create gpiotest.py

---
+++ gpiotest.py
@@ -0,0 +1,11 @@
+from gpiozero import Motor
+import time
+motor = Motor(forward=12, backward=20)
+actuator = Motor(forward=5, backward=6)
+while True:
+    motor.forward(speed=0.3)
+    actuator.forward()
+    time.sleep(2)
+    motor.backward(speed=0.3)
+    actuator.backward()
+    time.sleep(2)
